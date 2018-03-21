32fbda92cd293c8a8118e3e2f9fec5e346d96608
dotwoo <dotwoo@gmail.com>
2018-03-21 14:28:44 +0800

    添加测试用例

--- minmax.go
+++ minmax.go
@@ -6,36 +6,34 @@ type minMaxSample struct {
 }
 
 // WinMinMax 结构体
-type WinMinMax struct {
-	s [3]minMaxSample
-}
+type WinMinMax [3]minMaxSample
 
 // Get 返回最大或最小值
 func (w *WinMinMax) Get() uint32 {
-	return w.s[0].v
+	return w[0].v
 }
 
 // Reset 重置结构
 func (w *WinMinMax) Reset(t, meas uint32) uint32 {
-	w.s[0].t, w.s[1].t, w.s[2].t = t, t, t
-	w.s[0].v, w.s[1].v, w.s[2].v = meas, meas, meas
-	return w.s[0].v
+	w[0].t, w[1].t, w[2].t = t, t, t
+	w[0].v, w[1].v, w[2].v = meas, meas, meas
+	return w[0].v
 }
 
 // RunningMax 更新最大值
 func (w *WinMinMax) RunningMax(win, t, meas uint32) uint32 {
 	val := minMaxSample{t, meas}
 
-	if meas >= w.s[0].v || /* found new max? */
-		t-w.s[2].t > win { /* nothing left in window? */
+	if meas >= w[0].v || /* found new max? */
+		t-w[2].t > win { /* nothing left in window? */
 		return w.Reset(t, meas) /* forget earlier samples */
 	}
 
-	if meas >= w.s[1].v {
-		w.s[2], w.s[1] = val, val
+	if meas >= w[1].v {
+		w[2], w[1] = val, val
 
-	} else if meas >= w.s[2].v {
-		w.s[2] = val
+	} else if meas >= w[2].v {
+		w[2] = val
 	}
 
 	return w.subWinUpdate(win, &val)
@@ -45,22 +43,22 @@ func (w *WinMinMax) RunningMax(win, t, meas uint32) uint32 {
 func (w *WinMinMax) RunningMin(win, t, meas uint32) uint32 {
 	val := minMaxSample{t, meas}
 
-	if meas <= w.s[0].v || /* found new min? */
-		t-w.s[2].t > win { /* nothing left in window? */
+	if meas <= w[0].v || /* found new min? */
+		t-w[2].t > win { /* nothing left in window? */
 		return w.Reset(t, meas) /* forget earlier samples */
 	}
 
-	if meas <= w.s[1].v {
-		w.s[2], w.s[1] = val, val
-	} else if meas <= w.s[2].v {
-		w.s[2] = val
+	if meas <= w[1].v {
+		w[2], w[1] = val, val
+	} else if meas <= w[2].v {
+		w[2] = val
 	}
 
 	return w.subWinUpdate(win, &val)
 }
 
 func (w *WinMinMax) subWinUpdate(win uint32, val *minMaxSample) uint32 {
-	dt := val.t - w.s[0].t
+	dt := val.t - w[0].t
 
 	if dt > win {
 		/*
@@ -70,26 +68,26 @@ func (w *WinMinMax) subWinUpdate(win uint32, val *minMaxSample) uint32 {
 		 * may also be outside the window (we checked on entry
 		 * that the third choice was in the window).
 		 */
-		w.s[0] = w.s[1]
-		w.s[1] = w.s[2]
-		w.s[2] = *val
-		if val.t-w.s[0].t > win {
-			w.s[0] = w.s[1]
-			w.s[1] = w.s[2]
-			w.s[2] = *val
+		w[0] = w[1]
+		w[1] = w[2]
+		w[2] = *val
+		if val.t-w[0].t > win {
+			w[0] = w[1]
+			w[1] = w[2]
+			w[2] = *val
 		}
-	} else if w.s[1].t == w.s[0].t && dt > win/4 {
+	} else if w[1].t == w[0].t && dt > win/4 {
 		/*
 		 * We've passed a quarter of the window without a new val
 		 * so take a 2nd choice from the 2nd quarter of the window.
 		 */
-		w.s[2], w.s[1] = *val, *val
-	} else if w.s[2].t == w.s[1].t && dt > win/2 {
+		w[2], w[1] = *val, *val
+	} else if w[2].t == w[1].t && dt > win/2 {
 		/*
 		 * We've passed half the window without finding a new val
 		 * so take a 3rd choice from the last half of the window
 		 */
-		w.s[2] = *val
+		w[2] = *val
 	}
-	return w.s[0].v
+	return w[0].v
 }
--- minmax_test.go
+++ minmax_test.go
@@ -4,109 +4,180 @@ import (
 	"testing"
 )
 
-func TestWinMinMax_Get(t *testing.T) {
-	type fields struct {
-		s [3]minMaxSample
+func TestWinMinMax_Reset(t *testing.T) {
+	type args struct {
+		t    uint32
+		meas uint32
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		want   uint32
+		name string
+		w    *WinMinMax
+		args args
+		want uint32
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "reset1",
+			w:    &WinMinMax{},
+			args: args{1, 1},
+			want: 1,
+		},
+		{
+			name: "reset2",
+			w:    &WinMinMax{},
+			args: args{1, 2},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &WinMinMax{
-				s: tt.fields.s,
+			if got := tt.w.Reset(tt.args.t, tt.args.meas); got != tt.want {
+				t.Errorf("WinMinMax.Reset() = %v, want %v", got, tt.want)
 			}
-			if got := w.Get(); got != tt.want {
+			if tt.w[0].t != tt.args.t || tt.w[1].t != tt.args.t || tt.w[2].t != tt.args.t {
+				t.Errorf("WinMinMax.Reset()  the t %v, want %v", tt.w[0].t, tt.args.t)
+			}
+			if tt.w[0].v != tt.args.meas || tt.w[1].v != tt.args.meas || tt.w[2].v != tt.args.meas {
+				t.Errorf("WinMinMax.Reset()  the v %v, want %v", tt.w[0].v, tt.args.meas)
+			}
+		})
+	}
+}
+
+func TestWinMinMax_Get(t *testing.T) {
+	tests := []struct {
+		name string
+		w    *WinMinMax
+		want uint32
+	}{
+		// TODO: Add test cases.
+		{
+			name: "get1",
+			w:    &WinMinMax{{1, 1}, {0, 0}, {2, 2}},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.w.Get(); got != tt.want {
 				t.Errorf("WinMinMax.Get() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestWinMinMax_Reset(t *testing.T) {
-	type fields struct {
-		s [3]minMaxSample
-	}
-	type args struct {
-		t    uint32
-		meas uint32
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   uint32
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			w := &WinMinMax{
-				s: tt.fields.s,
-			}
-			if got := w.Reset(tt.args.t, tt.args.meas); got != tt.want {
-				t.Errorf("WinMinMax.Reset() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestWinMinMax_RunningMax(t *testing.T) {
-	type fields struct {
-		s [3]minMaxSample
-	}
 	type args struct {
 		win  uint32
 		t    uint32
 		meas uint32
+		time uint32
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   uint32
+		name string
+		w    *WinMinMax
+		args args
+		want uint32
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "runningmax10",
+			w:    &WinMinMax{{3, 3}, {2, 2}, {1, 1}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 10,
+			},
+			want: 10,
+		},
+		{
+			name: "runningmax11",
+			w:    &WinMinMax{{3, 11}, {2, 2}, {1, 1}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 10,
+			},
+			want: 11,
+		},
+		{
+			name: "runningmax11_10times",
+			w:    &WinMinMax{{3, 11}, {2, 2}, {1, 1}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 10,
+				time: 10,
+			},
+			want: 10,
+		},
+		{
+			name: "runningmax11_20times",
+			w:    &WinMinMax{{3, 40}, {2, 2}, {1, 1}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 10,
+				time: 20,
+			},
+			want: 10,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &WinMinMax{
-				s: tt.fields.s,
+			var got uint32
+			for i := uint32(0); i <= tt.args.time; i++ {
+				got = tt.w.RunningMax(tt.args.win, tt.args.t+i, tt.args.meas+i)
 			}
-			if got := w.RunningMax(tt.args.win, tt.args.t, tt.args.meas); got != tt.want {
-				t.Errorf("WinMinMax.RunningMax() = %v, want %v", got, tt.want)
+			if got != tt.want+tt.args.time {
+				t.Errorf("WinMinMax.RunningMax() = %v, want %v", got, tt.want+tt.args.time)
 			}
 		})
 	}
 }
 
 func TestWinMinMax_RunningMin(t *testing.T) {
-	type fields struct {
-		s [3]minMaxSample
-	}
 	type args struct {
 		win  uint32
 		t    uint32
 		meas uint32
+		time uint32
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   uint32
+		name string
+		w    *WinMinMax
+		args args
+		want uint32
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
+		{
+			name: "runningmin1",
+			w:    &WinMinMax{{3, 1}, {2, 2}, {1, 1}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 10,
+			},
+			want: 1,
+		},
+		{
+			name: "runningmin1",
+			w:    &WinMinMax{{3, 2}, {2, 3}, {1, 4}},
+			args: args{
+				win:  10,
+				t:    4,
+				meas: 1,
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &WinMinMax{
-				s: tt.fields.s,
+			var got uint32
+			for i := uint32(0); i <= tt.args.time; i++ {
+				got = tt.w.RunningMin(tt.args.win, tt.args.t+i, tt.args.meas+i)
 			}
-			if got := w.RunningMin(tt.args.win, tt.args.t, tt.args.meas); got != tt.want {
+			if got != tt.want {
 				t.Errorf("WinMinMax.RunningMin() = %v, want %v", got, tt.want)
 			}
 		})
